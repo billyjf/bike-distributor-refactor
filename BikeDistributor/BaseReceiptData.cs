c8eb73009cccbe678be57402409059c23de8d297
Billy Fisher <billyjfisher@gmail.com>
2015-09-20 12:52:16 -0700

    Added BaseReceiptData and TextReceipt used in last two commits.

---
+++ BikeDistributor/BaseReceiptData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeDistributor
+{
+    public class BaseReceiptData
+    {
+        public string m_Company;
+        public string m_totalAmount;
+        public TupleList<Line, string> m_reportLines;
+        public string m_tax;
+        public string m_total;
+
+        public BaseReceiptData(string company,
+                               string totalAmount,
+                               TupleList<Line, string> reportLines,
+                               string tax,
+                               string total)
+        {
+            this.m_Company = company;
+            this.m_totalAmount = totalAmount;
+            this.m_reportLines = reportLines;
+            this.m_tax = tax;
+            this.m_total = total;
+        }
+    }
+}
---
+++ BikeDistributor/TextReceiptData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeDistributor
+{
+    public partial class TextReceipt
+    {
+        private BaseReceiptData data;
+
+        public TextReceipt(BaseReceiptData data)
+        {
+            this.data = data;
+        }
+    }
+}
